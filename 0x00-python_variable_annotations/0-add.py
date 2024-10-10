a869d0d37163be2af6824704c4ed26492c5e83b8
Normich ITOUA <nitoua.dev@gmail.com>
2024-10-10 10:48:02 -0700

    Add task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module for adding two floating-point numbers."""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Add two floating-point numbers.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of a and b.
+    """
+    return a + b
