289eb1063904b737c2952a0d55febd08da7676aa
Normich ITOUA <nitoua.dev@gmail.com>
2024-10-10 10:51:28 -0700

    Add task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Module for calculating the floor of a float."""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Calculate the floor of a float.
+
+    Args:
+        n (float): The input float number.
+
+    Returns:
+        int: The floor of the input float.
+    """
+    return math.floor(n)
