0539368846aa6f8757a4192e163185b956067272
Normich ITOUA <nitoua.dev@gmail.com>
2024-10-10 13:42:22 -0700

    U[date task 12

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -7,27 +7,21 @@ Functions:
         Returns a list with elements of the tuple repeated by the factor.
 """
 
-from typing import Tuple, List
+from typing import List, Tuple
 
 
-def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
-    """
-    Returns a list where each element in the tuple
-    lst is repeated factor times.
-
-    Args:
-        lst (Tuple[int, ...]): A tuple of integers to be repeated.
-        factor (int, optional): The number of times to repeat each element.
-
-    Returns:
-        List[int]: A list containing the repeated elements.
-    """
-    zoomed_in: List[int] = [item for item in lst for _ in range(factor)]
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    '''Creates multiple copies of items in a tuple.
+    '''
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(int(factor))
+    ]
     return zoomed_in
 
 
-array = (12, 72, 91)  # Modified to be a tuple as expected by the function
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3)  # Corrected to pass an integer, as expected
+zoom_3x = zoom_array(array, 3)
