a4c47514e120569f36ad7e72cb73aa29f8275fe6
Normich ITOUA <nitoua.dev@gmail.com>
2024-10-20 07:14:38 -0700

    Update task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,7 +6,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[float, None, None]:
     """
     Coroutine that yields 10 random numbers between 0 and 10.
     Each yield is preceded by an asynchronous wait of 1 second.
